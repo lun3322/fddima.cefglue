32280eb685814b9ca1aedf6be8ad8510d0094c37
fddima <fddima@localhost>
2011-09-15 23:49:37 +0300

    Marked as 0.4.4-cef-r275

--- CefGlue.Version.cs
+++ CefGlue.Version.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.3.0")]
-[assembly: AssemblyFileVersion("0.4.3.0")]
-[assembly: AssemblyInformationalVersion("0.4.3-cef-r275")]
+[assembly: AssemblyVersion("0.4.4.0")]
+[assembly: AssemblyFileVersion("0.4.4.0")]
+[assembly: AssemblyInformationalVersion("0.4.4-cef-r275")]
